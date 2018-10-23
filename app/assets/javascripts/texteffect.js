7a464f7618f9803a2183ef2c44f88c77241771da
Briana Gude <briana.gude@gmail.com>
2018-10-22 21:30:58 -0400

    checking javascript

--- app/assets/javascripts/texteffect.js
+++ app/assets/javascripts/texteffect.js
@@ -1,12 +1,19 @@
-const makeSpans = selector => {
-  const [...elements] = document.querySelectorAll(selector)
-  return elements.map(element => {
-    const text = element.innerText.split('')
-    const spans = text
-      .map(letter => '<span>' + letter + '</span>')
-      .join('')
-    return element.innerHTML = spans
-  })
-}
+  const makeSpans = selector => {
+    const [...elements] = document.querySelectorAll(selector)
+    return elements.map(element => {
+      const text = element.innerText.split('')
+      const spans = text
+        .map(letter => '<span>' + letter + '</span>')
+        .join('')
+      return element.innerHTML = spans
+    })
+  }
 
-makeSpans('p, h1, h2')
+  makeSpans('p, h1, h2')
+
+
+  const one = document.querySelectorAll('p span:nth-child(1n)')
+
+  one.addEventListener('touchstart', function(){
+    one.style.color = '#F92A80'
+  })
